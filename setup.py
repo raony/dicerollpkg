e920f941566ab8a8f859c626cc9ed7218fa663e0
nico <raraujo@thoughtworks.com>
2016-05-16 00:36:35 -0300

    adding the toplevel module

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(
     name = 'diceroll',
     version = '0.1',
     packages = find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
+    py_modules=['diceroll'],
     test_suite = 'tests',
     tests_require = ['mock'],
 )
